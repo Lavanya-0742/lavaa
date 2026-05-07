8b3bacc0fe09fb34814a167f5892913e2fcff491
Lavanya-0742 <lav2007anya@gmail.com>
2026-05-08 00:08:11 +0530

    Update src.script.js

--- src.script.js
+++ src.script.js
@@ -31,7 +31,7 @@ btn.addEventListener("click", async () => {
       let average = sum / dataArray.length;
 
       // 4. Check the volume level
-      if (average > 0.10) {
+      if (average > 0.18) {
         // Put out flame
         flame.classList.add("extinguished");
 
